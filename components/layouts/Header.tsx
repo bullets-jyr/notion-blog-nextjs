08574091482d6d50a58a346f80edea6bd1815f92
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-07 19:29:18 +0900

    블로그 상세 목차 UI

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -3,20 +3,22 @@ export default function Header() {
     return (
         <header className="bg-background sticky top-0 z-50 border-b">
             <div className="container flex h-14 items-center px-4">
-                <Link href="/" className="text-xl font-semibold">
-                    <span className="font-bold">Bullets 블로그</span>
-                </Link>
-                <nav className="ml-auto flex items-center gap-4">
-                    <Link href="/" className="hover:text-primary font-medium">
-                        홈
+                <div className="container flex h-[var(--header-height)] items-center px-4">
+                    <Link href="/" className="text-xl font-semibold">
+                        <span className="font-bold">Bullets 블로그</span>
                     </Link>
-                    <Link href="/blog" className="hover:text-primary font-medium">
-                        블로그
-                    </Link>
-                    <Link href="/about" className="hover:text-primary font-medium">
-                        소개
-                    </Link>
-                </nav>
+                    <nav className="ml-auto flex items-center gap-4">
+                        <Link href="/" className="hover:text-primary font-medium">
+                            홈
+                        </Link>
+                        <Link href="/blog" className="hover:text-primary font-medium">
+                            블로그
+                        </Link>
+                        <Link href="/about" className="hover:text-primary font-medium">
+                            소개
+                        </Link>
+                    </nav>
+                </div>
             </div>
         </header>
     );
